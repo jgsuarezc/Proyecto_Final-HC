8e584bd948115073053c53848789e94c702866f8
jgsuarezc <jgsuarezc@unal.edu.co>
2022-06-30 15:46:51 -0500

    Paralelización

--- N-body.cpp
+++ N-body.cpp
@@ -19,9 +19,15 @@ void posicion (std::vector<Particulas> &planeta,int N,double seed);//posición (
 //serie
 void FuerzaT (std::vector<Particulas> &planeta,int N);//Fuerza Total
 void imprimir(const std::vector<Particulas> &planeta,int N);// imprime en pantalla la posicion y la fuerza
-//paralelo
-void FParallelo(std::vector<Particulas> &planeta,int N,int pid,int np);//Fuerza Total
-void imprimir(const std::vector<Particulas> &planeta,int N,int pid,int np);// imprime en pantalla la posicion y la fuerza
+//void FuerzaT2(double % M,const std::vector<Particulas> planeta, int N);//fuerza total array
+// imprimir2(const double M,int N);
+
+void XManda(std::vector<Particulas> PR,int pid ,int np);
+void XRecibe(std::vector<Particulas> PR,std::vector<Particulas> &Bu,int pid ,int np);
+
+void FParallelo(std::vector<Particulas> &planeta,int N,int pid,int np);
+
+
 
 
 int main(int argc, char *argv[]) {
@@ -37,14 +43,14 @@ int main(int argc, char *argv[]) {
   FuerzaT(planeta,N);
   imprimir(planeta,N);
 
+
+
   MPI_Init(&argc, &argv);
   int pid, np;
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
-
   MPI_Comm_size(MPI_COMM_WORLD, &np);
 
   MPI_Finalize();
-
 
 
 
@@ -62,7 +68,7 @@ double fuerza(double m1,double m2,double d){
 
 return F;
 }
-// genera una posicion x e y aleatoria para las particulas
+//genera una posicion x e y aleatoria para las particulas
 void posicion (std::vector<Particulas> &planeta,int N,double seed){
 
   std::mt19937 gen(seed);
@@ -91,7 +97,7 @@ void FuerzaT (std::vector<Particulas> &planeta,int N){
           double dy=planeta[ii].y-planeta[jj].y;//distancia yi-yj
           double d= sqrt(dx*dx+dy*dy); // distancia al cuadrado
           sum=sum + fuerza(M1T,M2T,d);//variable va sumando la fuerza sobre la particula Mi debida a las Mj con j distinto que i
-    }
+    }//fuerza x e y
     planeta[ii].F=sum;
   }
 
@@ -103,10 +109,40 @@ void imprimir(const std::vector<Particulas> &planeta,int N){
   }
 }
 
+/*
+void FuerzaT2(double & M,const std::vector<Particulas> planeta,int N){ //fuerza total array
+
+  for (int ii = 0; ii < N; ii++) {
+    double sum=0;
+    for(int jj = 0; jj < N; jj++){
+
+      // para una particula suma las fuerzas debida a las demas  excepto ella misma
+        if(jj==ii){M[ii+N*jj]=0}
+          double M1T=planeta[ii].M;
+          double M2T=planeta[jj].M;
+          double dx=planeta[ii].x-planeta[jj].x;//distancia xi-xj
+          double dy=planeta[ii].y-planeta[jj].y;//distancia yi-yj
+          double d= sqrt(dx*dx+dy*dy); // distancia al cuadrado
+          double d3=sqrt(d*d)
+          M[ii+N*jj]= fuerza(M1T,M2T,d);// guarda en un arreglo un la fuerza sobre i debido a j
+    }
+  }
+}
+
+
+void imprimir2(const double M,int N){
+  for (int ii = 0; ii< N; ++ii){
+    for(int jj=0;jj<N;++jj)
+    // imprime la posicion x e y junto a la fuerza
+    std::cout << M[ii+N*jj] <<std::endl;
+  }
+}
+
+*/
 
 // paralelizado
 // Suma lafuerza en paralelo
-void FParallelo(std::vector<Particulas> &planeta,int N,int pid,int np);
+void FParallelo(std::vector<Particulas> &planeta,int N,int pid,int np)
 {
 
   const int nitems=6;
@@ -114,31 +150,78 @@ void FParallelo(std::vector<Particulas> &planeta,int N,int pid,int np);
   MPI_Datatype types[6] = {MPI_DOUBLE,MPI_DOUBLE,MPI_DOUBLE,MPI_DOUBLE,MPI_DOUBLE,MPI_DOUBLE};
   MPI_Datatype mpi_struct_type;//Nuevo Tipo de dato en MPI
   MPI_Aint offset[6];
-  offsets[0] = offsetof(Particulas, x);
-  offsets[1] = offsetof(Particulas, y);
-  offsets[2] = offsetof(Particulas, Vx);
-  offsets[3] = offsetof(Particulas, Vy);
-  offsets[4] = offsetof(Particulas, F);
-  offsets[5] = offsetof(Particulas, M);
-
+  offset[0] = offsetof(Particulas, x);
+  offset[1] = offsetof(Particulas, y);
+  offset[2] = offsetof(Particulas, Vx);
+  offset[3] = offsetof(Particulas, Vy);
+  offset[4] = offsetof(Particulas, F);
+  offset[5] = offsetof(Particulas, M);
   MPI_Type_create_struct(nitems, len, offset, types, &mpi_struct_type);
   MPI_Type_commit(&mpi_struct_type);
+  int tag =0;
+  std::vector<Particulas>P;
+  P.resize(N/np);
 
-
-  //valor
   std::vector<Particulas>buffer;
-  std::vector<Particulas>tag;
+  buffer.resize(N/np);
 
-    int next = (pid+1)%np;
-    int prev = (pid-1+np)%np;
-    if (pid == 0) {
-      MPI_Send(&planeta, 1,MPI_DOUBLE, next, tag, MPI_COMM_WORLD);
+  std::vector<double> Fuerzas;
+  Fuerzass.resize(N/np);
 
-      MPI_Recv(&tag, 1,MPI_DOUBLE, prev, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    } else {
-      MPI_Recv(&planeta, 1,MPI_DOUBLE, prev, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  int next = (pid+1)%np;
+  int prev = (pid-1+np)%np;
+  if (pid == 0){
+      XManda(P,pid,np);
+      MPI_Send(&P, N/np,mpi_struct_type, next, tag, MPI_COMM_WORLD);
+      MPI_Recv(&buffer, N/np,mpi_struct_type, prev, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-      MPI_Send(&buf, 1, MPI_DOUBLE, next, tag, MPI_COMM_WORLD);
+    }
+     else {
+      XManda(P,pid,np);
+      MPI_Recv(&P,N/np,mpi_struct_type, prev, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      XRecibe(P,buffer,pid,np);
+      MPI_Send(&buffer, N/np,mpi_struct_type , next, tag, MPI_COMM_WORLD);
+    }
+
+}
+
+//Funcion calcula las fuerzas de las particulas perteneciente al proceso
+void XManda(std::vector<Particulas> PR,int pid ,int np){
+  double sum;
+//fuerza debida a las n  particulas propias del proceso
+for (int ii = N*pid/np; ii < N*(pid+1)/np; ii++) {
+  double sum=0;
+  for(int jj = N*pid/np; jj <N*(pid+1)/np; jj++){
+      // para una particula suma las fuerzas debida a las demas  excepto ella misma
+      if(jj==ii){continue;}
+        double M1T=PR[ii].M;
+        double M2T=PR[jj].M;
+        double dx=PR[ii].x-planeta[jj].x;//distancia xi-xj
+        double dy=planeta[ii].y-planeta[jj].y;//distancia yi-yj
+        double d= sqrt(dx*dx+dy*dy); // distancia al cuadrado
+        sum=sum + fuerza(M1T,M2T,d);//variable va sumando la fuerza sobre la particula Mi debida a las Mj con j distinto que i
+
+      }
+
+  }
+
+//calcula la fuerza debida a los datos recibido del proceso anterior guardos  en buffer
+}
+void XRecibe(std::vector<Particulas> PR,std::vector<Particulas> Bu,int pid ,int np){
+
+  //fuerza debida a las n  particulas propias del proceso
+  for (int ii = 0; ii <= N*(pid+1)/np; ii++) {
+    double sum=0;
+    for(int jj = N/np; jj <=(N+1)/Np; jj++){
+        // para una particula suma las fuerzas debida a las demas  excepto ella misma
+          double M1T=Bu[ii].M;
+          double M2T=PR[jj].M;
+          double dx=Bu[ii].x-planeta[jj].x;//distancia xi-xj
+          double dy=PR[ii].y-planeta[jj].y;//distancia yi-yj
+          double d= sqrt(dx*dx+dy*dy); // distancia al cuadrado
+          sum=sum + fuerza(M1T,M2T,d);//variable va sumando la fuerza sobre la particula Mi debida a las Mj con j distinto que i
+
+        }
     }
 
 }
