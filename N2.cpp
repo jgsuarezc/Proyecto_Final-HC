eaf92b273119d4ed57b216bd726eb280ce1120a7
jgsuarezc <jgsuarezc@unal.edu.co>
2022-07-06 03:10:20 -0500

    ring

--- N2.cpp
+++ N2.cpp
@@ -41,7 +41,7 @@ int np;  /* number of processes */
 MPI_Comm_size(MPI_COMM_WORLD, &np);
 MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
-ring(pid, np);
+ring(posiciones,N,pid, np);
 
 MPI_Finalize(); /* Mandatory */
 
